dc940f93ee3c2b2801378af88feca3bc23ba2bec
Logan Domingo <logankdomingo@gmail.com>
2022-04-17 21:15:01 -0600

    initial commit

--- server.py
+++ server.py
@@ -1,32 +1,7 @@
-from flask import Flask, render_template, request, redirect, session
-app = Flask(__name__)
-app.secret_key = 'He said to blathe'
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/process', methods=['POST'])
-def process():
-    session['name'] = request.form['name']
-    session['language'] = request.form['favoriteLang']
-    session['comment'] = request.form['comment']
-
-    if request.form['Seattle'] == 'on':
-        session['location'] = 'Seattle'
-        return redirect('/result')
-    if request.form['SanJose'] == 'on':
-        session['location'] = 'San Jose'
-        return redirect('/result')
-    if request.form['Burbank'] == 'on':
-        session['location'] = 'Burbank'
-        return redirect('/result')
-
-@app.route('/result')
-def result():
-
-    return render_template('result.html')
+from flask_app import app
+from flask_app.controllers import dojos_controller
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
